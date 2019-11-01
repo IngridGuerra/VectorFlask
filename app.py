d334026cf4a6846bf77cba6a20fa5becfab030e5
IngridGuerra <43792782+IngridGuerra@users.noreply.github.com>
2019-11-01 16:37:14 -0600

    tableView added

--- app.py
+++ app.py
@@ -10,26 +10,30 @@ def home():
     return render_template("index.html",
     data = [{'query': 'what similarity laws must be obeyed'}, {'query': 'what are the structural and aeroelastic'}, {'query': 'can a criterion be developed to show'}])
 
-@app.route("/getQueries", methods = ['GET'])
-def sendQueries():
-    query_dict = {'1': 'first query', '2': 'second query', '3': 'third query'}
-    return jsonify(queries=query_dict)
+#@app.route("/getQueries", methods = ['GET'])
+#def sendQueries():
+#    query_dict = {'1': 'first query', '2': 'second query', '3': 'third query'}
+#    return jsonify(queries=query_dict)
 
-@app.route("/saveForm", methods = ['POST'])
-def save_user():
-    #use request.form when accesing form data
-    # each field of request.form['...'] is the name of the input on the HTML.
-    print('ID Estudiante: '     + request.form['id'])
-    print('Nombre Estudiante: ' + request.form['nombre'])
-    print('Correo Estudiante: ' + request.form['correo'])
-    return 'Estudiante Guardado'
+#@app.route("/saveForm", methods = ['POST'])
+#def save_user():
+#    #use request.form when accesing form data
+#    # each field of request.form['...'] is the name of the input on the HTML.
+#    print('ID Estudiante: '     + request.form['id'])
+#    print('Nombre Estudiante: ' + request.form['nombre'])
+#    print('Correo Estudiante: ' + request.form['correo'])
+#    return 'Estudiante Guardado'
 
-@app.route("/form")
-def form():
-    return render_template('form.html')
+#@app.route("/form")
+#def form():
+#    return render_template('form.html')
 
 @app.route("/selectQuery", methods=['GET','POST'])
 def saveQuery():
     select = request.form.get('queries')
     selectedQry = str(select)
-    return vectorspace.start(selectedQry) #to see what select is
+    vectorspace.start(selectedQry) #to see what select is
+    return render_template('table.html')
+
+if __name__ == '__main__':
+    app.debug = True
