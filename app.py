210d49cd1051590ac2b763cf564e0860180cab62
IngridGuerra <43792782+IngridGuerra@users.noreply.github.com>
2019-11-05 16:22:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
 from flask import Flask, render_template
 from flask import request 
 from flask import jsonify
-import json
+import os
 import vectorspace
 import precisionrecall
 
 app = Flask(__name__)
-#resultData = open("output.json", "r")
-#data = resultData.read()
 columns = ['Num Document', 'Title', 'Match Sentence', 'Ranking Coefficient']
-'''columns = [{
-    "field": "No. Document", # which is the field's name of data key 
-    "title": "No. Document", # display as the table header's name
-    "sortable": True,
-  },
-  {
-    "field": "Title",
-    "title": "Title",
-    "sortable": True,
-  },
-  {
-    "field": "Sentence",
-    "title": "Sentence",
-    "sortable": False,
-  },
-  {
-    "field": "Ranking coefficient",
-    "title": "Ranking coefficient",
-    "sortable": True,
-  }]'''
 
 @app.route("/")
 @app.route("/clear")
@@ -36,26 +14,8 @@ def home():
     return render_template("index.html",
     qSet = [{'query': 'what similarity laws must be obeyed'}, {'query': 'what are the structural and aeroelastic'}, {'query': 'can a criterion be developed to show'}])
 
-#@app.route("/getQueries", methods = ['GET'])
-#def sendQueries():
-#    query_dict = {'1': 'first query', '2': 'second query', '3': 'third query'}
-#    return jsonify(queries=query_dict)
-
-#@app.route("/saveForm", methods = ['POST'])
-#def save_user():
-#    #use request.form when accesing form data
-#    # each field of request.form['...'] is the name of the input on the HTML.
-#    print('ID Estudiante: '     + request.form['id'])
-#    print('Nombre Estudiante: ' + request.form['nombre'])
-#    print('Correo Estudiante: ' + request.form['correo'])
-#    return 'Estudiante Guardado'
-
-#@app.route("/form")
-#def form():
-#    return render_template('form.html')
-
 @app.route("/selectQuery", methods=['GET'])
-def saveQuery():
+def selectQuery():
     select = request.form.get('queries')
     selectedQry = str(select) #to see what select is
     return selectedQry
